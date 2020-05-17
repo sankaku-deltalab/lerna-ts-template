b3e90c92b275cab193016158d918934bb8778546
Sankaku <sankaku_dlt.45631@outlook.jp>
2020-05-17 21:03:47 +0900

    Test exclude files like "a.ts" not "a.test.ts"

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
   collectCoverageFrom: ["packages/**/src/**/*.ts"]
 };
